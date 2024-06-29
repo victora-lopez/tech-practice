35920e98d6d2a8ebcb6891aec277a4b7ff4c0644
Victor Lopez <victorandreslopez314@gmail.com>
2024-06-28 19:57:20 -0500

    Create gameChoices function
    gameChoices function will contain an array
    which holds the strings rock, paper, scissors.
    The function takes the parameter choiceNum and
    return the choice based on the arrays index.
    choiceNum will be retreived from user in
    getHumanChoice() function and decremented since
    it will be retrieved from a prompt menu.

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function gameChoices(choiceNum){
+    const choices = ["rock", "paper", "scissors"];
+    return choices[choiceNum - 1];
+}
+
+function getComputerChoice(){
+
+}
+
+function getHumanChoice(){
+
+}
+
+function playRound(humanChoice, computerChoice){
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
