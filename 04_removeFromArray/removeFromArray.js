61124a64e6d100bd4f2ff4f7bd98947e597b4185
Victor Lopez <victorandreslopez314@gmail.com>
2024-07-01 20:06:28 -0500

    Solve exercise 4 from javascript exercises
    Learned how to use filter, and includes method along
    with arrow functions. arrow function is commonly used
    with filter method to take specific values out. include
    was used to see if the value to be removed was in the
    array passed to the function. the ! (not operator) was
    used to negate the falsy values to truthy because we
    only want to pass values that ARENT in the remove
    argument. with .includes we are looking for the values
    in the remove argument.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    return array.filter(val => !remove.includes(val));
 };
 
 // Do not edit below this line
