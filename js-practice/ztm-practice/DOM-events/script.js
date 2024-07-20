796ef3d71c7144e1faad16b45593d7fa4c113993
Victor Lopez <victorandreslopez314@gmail.com>
2024-07-20 14:59:55 -0500

    Create new directory for ZTM practice
    In the new directory, I will practice
    Javascript and DOM manipulation. Holds
    the file I was working on to practice
    event listeners.

---
+++ js-practice/ztm-practice/DOM-events/script.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+var li = document.querySelectorAll("li");
+var deleteButton = document.createElement("button");
+deleteButton.appendChild(document.createTextNode("Delete"));
+
+li[0].appendChild(deleteButton)
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
