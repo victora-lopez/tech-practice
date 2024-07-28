beb0bd603e1e58f6d3da629ebd3acaa1bbaf109d
Victor Lopez <victorandreslopez314@gmail.com>
2024-07-28 02:25:42 -0500

    Add Sega font and style robofriends project

--- react-practice/robofriends/src/App.js
+++ react-practice/robofriends/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import { filter } from 'lodash';
+import './App.css';
 
 
 
@@ -10,9 +10,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+         .then(response => response.json())
+         .then(users => this.setState({ robots: users}))
     }
 
     onSearchChange= (event) => {
@@ -30,7 +36,7 @@ class App extends Component {
         });
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
