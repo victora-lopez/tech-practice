ecb57177b8158323538986b95b85a32ec571b240
Victor Lopez <victorandreslopez314@gmail.com>
2024-07-29 14:16:49 -0500

    Create scroll component for robofriends

--- react-practice/robofriends/src/containers/App.js
+++ react-practice/robofriends/src/containers/App.js
@@ -1,48 +1,46 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll.js';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 
-
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
     }
+  }
 
-    componentDidMount(){
-        fetch("https://jsonplaceholder.typicode.com/users")
-         .then(response => response.json())
-         .then(users => this.setState({ robots: users}))
-    }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
 
-    onSearchChange= (event) => {
-        this.setState({searchfield: event.target.value})
-        console.log(event.target.value);
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
-        console.log(filteredRobots);
-    }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
 
-    render(){
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <CardList robots={filteredRobots}/>
-                </Scroll>
-            </div>
-        );
-    }
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
+
 export default App;
--- react-practice/robofriends/src/index.js
+++ react-practice/robofriends/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
-import CardList from './components/CardList'
 import 'tachyons';
-import { robots } from './robots';
+// import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
